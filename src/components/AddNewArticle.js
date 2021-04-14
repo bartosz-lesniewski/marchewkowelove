58da4b5b79b032b0d129ec89b1a4ccbc312b62df
bartosz-lesniewski <b.lesniewski8@gmail.com>
2021-04-14 18:11:22 +0200

    addnewarticle css added

--- src/components/AddNewArticle.js
+++ src/components/AddNewArticle.js
@@ -1,26 +1,33 @@
+import "../styles/addnewarticle.css";
 import React from "react";
 
 function AddNewArticle() {
 	return (
-		<div>
-			<label for="article--title">Tytuł:</label>
-			<input
-				className="article--title"
-				placeholder="Tutaj wpisz tytuł artykułu:"
-				required
-			/>
-			<label for="article--lid">Lid:</label>
-			<input
-				className="article--lid"
-				placeholder="Tutaj wpisz akapit wprowadzający:"
-				required
-			/>
-			<label for="article--content">Treść:</label>
-			<input
-				className="article--content"
-				placeholder="Tutaj wpisz treść właściwą artykułu:"
-				required
-			/>
+		<div className="article--container">
+			<form className="title--container">
+				<label for="article--title">Tytuł:</label>
+				<input
+					className="article--title"
+					placeholder="Tutaj wpisz tytuł artykułu:"
+					required
+				/>
+			</form>
+			<form className="lid--container">
+				<label for="article--lid">Lid:</label>
+				<input
+					className="article--lid"
+					placeholder="Tutaj wpisz akapit wprowadzający:"
+					required
+				/>
+			</form>
+			<form className="content--container">
+				<label for="article--content">Treść:</label>
+				<input
+					className="article--content"
+					placeholder="Tutaj wpisz treść właściwą artykułu:"
+					required
+				/>
+			</form>
 			<button>Opublikuj</button>
 		</div>
 	);
